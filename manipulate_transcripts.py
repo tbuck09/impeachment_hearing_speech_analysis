c7977cedca44f423cffd74902aa8d3e3036ea043
tbuck09 <tylerbuck09@gmail.com>
2019-12-03 21:34:38 -0700

    Plotted (approximate) total durations for each speaker

--- manipulate_transcripts.py
+++ manipulate_transcripts.py
@@ -2,12 +2,16 @@ import os
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
 import nltk
 
 from util import timestamp_to_int
 
 
 # Establish paths and variables
+cwd= os.getcwd()
+plot_path= os.path.join(cwd,"plots")
 transcript_filename= "Hearings_data.csv"
 
 
@@ -19,7 +23,7 @@ transcript_df= pd.read_csv(transcript_filename, encoding='latin1')
 ## Integer-ized timestamps
 transcript_df["int_timestamp"]= transcript_df.Timestamp.apply(timestamp_to_int)
 ## Speaker Duration
-## At this time I don't have a way to mitigate when the timestamp resets to 00:00 within a hearing (such as a Section change).
+## At this time I don't have a way to mitigate when the timestamp resets to 00:00 within a hearing (such as a Hearing or Section change).
 ## These values are np.nan for now.
 transcript_df["duration"]= 0
 for i in range(len(transcript_df) - 1):
@@ -28,3 +32,6 @@ for i in range(len(transcript_df) - 1):
         transcript_df.duration[i]= np.nan
     else:
         transcript_df.duration[i]= duration
+
+
+# Plots
